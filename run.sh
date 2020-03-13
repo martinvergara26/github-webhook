4ac986834f8be2d778700e9ee8bb716d825f76e0
Martín Vergara <martinmorsevergara@gmail.com>
2020-03-13 14:28:01 -0300

    Changes node image to 10.19.0-alpine3.9

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker run -ti -p 9999:9999 -v $PWD:/app -w /app node:8 npm install
+docker run -ti -p 9999:9999 -v $PWD:/app -w /app node:10.19.0-alpine3.9 npm install
 
-docker run -ti -p 9999:9999 -v $PWD:/app -w /app --rm node:8 npm start
+docker run -ti -p 9999:9999 -v $PWD:/app -w /app --rm node:10.19.0-alpine3.9 npm start
