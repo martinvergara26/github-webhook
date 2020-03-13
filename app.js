deb9c30e1176885ae55991674feb3ac880cfc49f
Martín Vergara <martinmorsevergara@gmail.com>
2020-03-13 17:16:26 -0300

    Adds signature validation

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
-const exec = require('child_process').exec;
-
 const express = require('express');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const exec = require('child_process').exec;
+const crypto = require('crypto')
+
+// Calculate the X-Hub-Signature header value.
+function getSignature(buf) {
+  const hmac = crypto.createHmac("sha1", "martin");
+  hmac.update(buf, "utf-8");
+  return "sha1=" + hmac.digest("hex");
+}
+
+// Verify function compatible with body-parser to retrieve the request payload.
+// Read more: https://github.com/expressjs/body-parser#verify
+function verifyRequest(req, res, buf, encoding) {
+  var expected = req.headers['x-hub-signature'];
+  var calculated = getSignature(buf);
+  console.log("X-Hub-Signature:", expected, "Content:", "-" + buf.toString('utf8') + "-");
+  if (expected !== calculated) {
+    throw new Error("Invalid signature.");
+  } else {
+    console.log("Valid signature!");
+  }
+}
+
+// Express error-handling middleware function.
+// Read more: http://expressjs.com/en/guide/error-handling.html
+function abortOnError(err, req, res, next) {
+  if (err) {
+    console.log(err);
+    res.status(400).send({ error: "Invalid signature." });
+  } else {
+    next();
+  }
+}
 
 const app = express();
 
-app.use(bodyParser.json());
+// body-parser is the first Express middleware.
+app.use(bodyParser.json({ verify: verifyRequest }))
+
+// Add an error-handling Express middleware function
+// to prevent returning sensitive information.
+app.use(abortOnError);
 
 app.use(function (req, res, next) {
   const event = req.get("x-github-event");
