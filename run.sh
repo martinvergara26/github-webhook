b2f4fba42167543dc43d22bdd81c5d452c34950d
Martín Vergara <martinmorsevergara@gmail.com>
2020-03-20 12:10:03 -0300

    Runs container in github_webhook network

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-docker run -ti -p 9999:9999 --env-file .env -v $HOME:/app -w /app/webhook-server --rm node:10.19.0-alpine3.9 npm start
+docker network rm github_webhook
+
+docker run -ti -p 9999:9999 --env-file .env --network github_webhook -v $HOME:/app -w /app/webhook-server --rm node:10.19.0-alpine3.9 npm start
