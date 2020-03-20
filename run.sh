cccbdf687fcbcb007ce0e2283a7bdea1d8e2ffb0
Martín Vergara <martinmorsevergara@gmail.com>
2020-03-20 11:53:56 -0300

    Passes environment variables to container

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-docker run -ti -p 9999:9999 -v $HOME:/app -w /app/webhook-server --rm node:10.19.0-alpine3.9 npm install
-
-docker run -ti -p 9999:9999 -v $HOME:/app -w /app/webhook-server --rm node:10.19.0-alpine3.9 npm start
+docker run -ti -p 9999:9999 -v $HOME:/app -w /app/webhook-server \
+  -e GB_WEBHOOK_SECRET=$GB_WEBHOOK_SECRET \
+  -e DEPLOY_PATH=$DEPLOY_PATH \
+  -e DEPLOY_COMMAND=$DEPLOY_COMMAND \
+  --rm node:10.19.0-alpine3.9 npm start \
+  /
