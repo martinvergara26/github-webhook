70190d1a9631a9029242600f25f3c399b56f4b17
Martín Vergara <martinmorsevergara@gmail.com>
2020-03-20 12:01:56 -0300

    Adds .env file

--- run.sh
+++ run.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -ti -p 9999:9999 -v $HOME:/app -w /app/webhook-server \
-  -e GB_WEBHOOK_SECRET=$GB_WEBHOOK_SECRET \
-  -e DEPLOY_PATH=$DEPLOY_PATH \
-  -e DEPLOY_COMMAND=$DEPLOY_COMMAND \
-  --rm node:10.19.0-alpine3.9 npm start \
-  /
+docker run -ti -p 9999:9999 --env-file .env -v $HOME:/app -w /app/webhook-server --rm node:10.19.0-alpine3.9 npm start
